ced3a3e38d08472d2e45542ebcd1248253f0faf2
Nitin tyagi <nitin@Nitins-MacBook-Pro.local>
2020-02-03 12:56:39 +0530

    template variable

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import React from "react";
 import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return <h1>Hello {name}</h1>;
-  }
+	render() {
+		const { name } = this.props;
+		return <h1>Hello {name}</h1>;
+	}
 }
 
 export default hot(App);
